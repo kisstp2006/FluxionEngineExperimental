a3c6f438b1e4ad8a01e5f86393b90d34536a2461
TIGames <kisstp2006@gmail.com>
2026-07-05 21:37:17 +0200

    viewmodel base added for event

---
+++ FluxionEditor/Foundation/ProjectProperty.cs
@@ -0,0 +1,14 @@
+﻿using FluxionEditor.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Text;
+
+namespace FluxionEditor.Foundation
+{
+    class ProjectProperty : ViewModelBase
+    {
+
+    }
+}
---
+++ FluxionEditor/Foundation/ViewModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace FluxionEditor
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
