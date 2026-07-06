df24929390cd020162ae16abae9a472d96ddf5d2
TIGames <kisstp2006@gmail.com>
2026-07-06 14:56:00 +0200

    Scene Editor

---
+++ FluxionEditor/Editor/SceneEditor/SceneEditorView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace FluxionEditor.Editor;
+
+public partial class SceneEditorView : UserControl
+{
+    public SceneEditorView()
+    {
+        InitializeComponent();
+    }
+}
