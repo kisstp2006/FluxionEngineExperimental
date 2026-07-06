0ed25f7b102f69b36be19fa744c0f802bb17513d
TIGames <kisstp2006@gmail.com>
2026-07-06 13:06:13 +0200

    open project WIP

--- FluxionEditor/Views/ProjectManager/Controls/OpenProjectView.axaml.cs
+++ FluxionEditor/Views/ProjectManager/Controls/OpenProjectView.axaml.cs
@@ -1,6 +1,7 @@
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
+using FluxionEditor.Foundation;
 
 namespace FluxionEditor;
 
@@ -9,5 +10,27 @@ public partial class OpenProjectView : UserControl
     public OpenProjectView()
     {
         InitializeComponent();
+    }
+
+    private void Open_Button_Click(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        OpenSelectedProject();
+    }
+
+    private void OpenSelectedProject()
+    {
+        
+        var project = OpenProject.Open(projectsListBox.SelectedItem as ProjectData);
+
+        bool dialogResult = false;
+        if (project != null)
+        {
+            dialogResult = true;
+        }
+
+
+        // Close the parent dialog and return true = project created
+        if (this.VisualRoot is Window window)
+            window.Close(true);
     }
 }
