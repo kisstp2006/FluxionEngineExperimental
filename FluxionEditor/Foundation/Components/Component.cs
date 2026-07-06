04fe72325dcb85be36409e65e896533d3606e92e
TIGames <kisstp2006@gmail.com>
2026-07-06 20:03:27 +0200

    fix for CS7036

--- FluxionEditor/Foundation/Components/Component.cs
+++ FluxionEditor/Foundation/Components/Component.cs
@@ -6,17 +6,23 @@ using System.Text;
 
 namespace FluxionEditor.Foundation.Components
 {
-    
+    [DataContract]
     public class Component : ViewModelBase
     {
-        public GameObject owner { get; private set; }
+        public GameObject Owner { get; private set; }
 
+        [DataMember]
         public bool IsEnabled { get; set; } = true;
+
+        /// <summary>Parameterless constructor required by DataContractSerializer.</summary>
+        public Component()
+        {
+        }
 
         public Component(GameObject owner)
         {
             Debug.Assert(owner != null, "Owner cannot be null");
-            this.owner = owner;
+            Owner = owner;
         }
     }
 }
