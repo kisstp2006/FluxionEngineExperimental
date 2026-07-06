80b7881f6c29c8893892d49b59baca158ba6d46e
TIGames <kisstp2006@gmail.com>
2026-07-06 21:59:52 +0200

    add gj button

--- FluxionEditor/Editor/SceneEditor/ProjectLayoutView.axaml.cs
+++ FluxionEditor/Editor/SceneEditor/ProjectLayoutView.axaml.cs
@@ -2,6 +2,7 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using FluxionEditor.Foundation;
+using FluxionEditor.Foundation.Components;
 
 namespace FluxionEditor;
 
@@ -10,5 +11,13 @@ public partial class ProjectLayoutView : UserControl
     public ProjectLayoutView()
     {
         InitializeComponent();
+    }
+
+    private void On_AddGameObject_Button_Clicked(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        var btn = sender as Button;
+        var vm = btn?.DataContext as Scene;
+
+        vm.AddGameObjectCommand.Execute(new GameObject(vm) {Name ="GameObject" });
     }
 }
