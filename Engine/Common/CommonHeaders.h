fcd5e69fd3a9da7ea1d00e0b4d38254e72d3e44c
TIGames <kisstp2006@gmail.com>
2026-07-07 19:38:04 +0200

    Engine basics

---
+++ Engine/Common/CommonHeaders.h
@@ -0,0 +1,8 @@
+#pragma once
+
+//C/CPP
+#include<stdint.h>
+#include<assert.h>
+#include<typeinfo>
+
+#include "CustomTypes.h"
---
+++ Engine/Common/Id.h
@@ -0,0 +1,36 @@
+#pragma once
+#include "CommonHeaders.h"
+
+namespace fluxion::common::id {
+	using id_type = flu32;
+
+
+	constexpr flu32 generation_bits(10);
+	constexpr flu32 index_bits(sizeof(id_type)*8 -generation_bits);
+	constexpr id_type index_mask{ (id_type(1) << index_bits) -1 };
+	constexpr id_type generation_mask{ (id_type(1) << generation_bits) - 1 };
+	constexpr id_type id_mask{ id_type{-1} };
+
+
+	using generation_type = std::conditional_t<generation_bits <= 16, std::conditional_t<generation_bits <= 8, flu8, flu16>, flu32>;
+	static_assert(sizeof(generation_type) * 8 >= generation_bits);
+	static_assert(sizeof(id_type) - sizeof(generation_type) > 0);
+
+
+	inline bool isValid(id_type id) {
+		return id != id_mask;
+	}
+
+	inline id_type index(id_type id) {
+		return id & index_mask;
+	}
+	
+	inline id_type generation(id_type id) {
+		return (id >>index_bits) & generation_mask;
+	}
+	inline id_type new_generation(id_type id) {
+		const id_type generation(id::generation(id) + 1);
+		assert(generation < 255);
+		return index(id) | (generation << index_bits);
+	}
+}
