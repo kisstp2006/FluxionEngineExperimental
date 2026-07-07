bdec88d855f04bf0be80f8824b5d793a26d17706
TIGames <kisstp2006@gmail.com>
2026-07-07 21:31:50 +0200

    namespace decopuling

--- Engine/ECS/Components/Transform.h
+++ Engine/ECS/Components/Transform.h
@@ -1,17 +1,17 @@
 #pragma once
-#include"ECS\ECSCommon.h"
+#include "ECS/ECSCommon.h"
 
-namespace fluxion::ecs::trnasform {
+namespace fluxion::ecs::transform {
 	DEFINE_TYPED_ID(transform_id);
 
 	struct init_info
 	{
-		flu32 position[3]{};
-		flu32 rotation[4]{};
-		flu32 scale[3]{1.f,1.f,1.f};
+		flf32 position[3]{};
+		flf32 rotation[4]{};
+		flf32 scale[3]{ 1.f, 1.f, 1.f };
 	};
 
-	transform_id create_transform(const init_info&, game_object_id gameobject_id);
+	transform_id create_transform(const init_info& info, game_object_id gameobject_id);
 
 	void remove_transform(transform_id id);
 }
--- Engine/ECS/GameObject.h
+++ Engine/ECS/GameObject.h
@@ -5,13 +5,13 @@ namespace fluxion::ecs {
 #define INIT_INFO(component) namespace component { struct init_info; }
 	INIT_INFO (transform)
 #undef INIT_INFO
-	namespace game_object { 
-		struct gameobject_info
+	namespace game_object {
+		struct game_object_info
 		{
 			transform::init_info* transform{ nullptr };
 		};
 
-		game_object_id create_game_object(const gameobject_info& info);
+		game_object_id create_game_object(const game_object_info& info);
 		void remove_game_object(game_object_id id);
 		bool is_alive(game_object_id id);
 	}
