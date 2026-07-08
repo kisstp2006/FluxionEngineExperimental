a5f25ee050e3369780b595a650b2d23ce5bc61ab
TIGames <kisstp2006@gmail.com>
2026-07-08 18:17:28 +0200

    math V1

--- EngineExport/EngineAPI.cpp
+++ EngineExport/EngineAPI.cpp
@@ -11,6 +11,30 @@ using namespace fluxion;
 
 namespace {
 
+	struct transform_component
+	{
+		flu32 position[3];
+		flu32 rotation[3];
+		flu32 scale[3];
+
+
+		ecs::transform::init_info to_init_info() {
+			using namespace DirectX;
+			ecs::transform::init_info info;
+
+			memcpy(&info.position[0],&position[0],sizeof(flu32)*_countof(position));
+			memcpy(&info.scale[0], &scale[0], sizeof(flu32) * _countof(scale));
+			XMFLOAT3A rot{ &rotation[0] };
+			XMVECTOR quat{ XMQuaternionRotationRollPitchYawFromVector(XMLoadFloat3A(&rot)) };
+
+
+		}
+	};
+
+	struct gane_object_descriptor
+	{
+		transform_component transform;
+	};
 }
 
 EDITOR_INTERFACE id::id_type CreateGameObject(game_object_descriptor* e) {
