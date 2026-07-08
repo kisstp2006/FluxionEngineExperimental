c7ce25346576a8d45a620f4b9b072ca4051b9586
TIGames <kisstp2006@gmail.com>
2026-07-08 15:07:50 +0200

    ECStest

--- FluxionTestFramework/Tests/ECS/ECSTest.h
+++ FluxionTestFramework/Tests/ECS/ECSTest.h
@@ -1,14 +1,74 @@
 #pragma once
 #include "../../Test.hpp"
 #include "ECS/GameObject.h"
+#include "ECS/Components/Transform.h"
+
+#include<iostream>
+#include<ctime>
+
 
 using namespace fluxion;
+using namespace fluxion::ecs;
 
 
 class engine_test : public test
 {
 public:
-	bool init() override { return true; }
-	bool run() override { return true; }
-	bool stop() override { return true; }
+	bool init() override 
+	{
+		srand((flu32)time(nullptr));
+		return true; 
+	}
+	bool run() override 
+	{ 
+		do {
+			for (flu32 i{ 0 }; i < 1000; ++i) {
+				create_ramdom();
+				remove_ramdom();
+				num_game_objects = (flu32)gameobjects.size();
+			}
+
+		} while (getchar() != 'q');
+		
+		return true; 
+	}
+	bool stop() override { 
+		return true; 
+	}
+
+private:
+	void create_ramdom()
+	{
+		flu32 count = rand() % 30;
+		if (gameobjects.empty())
+		{
+			count = 1000;
+		}
+		transform::init_info transfom_info{};
+		game_object::game_object_info gameobject_info{
+			&transfom_info,
+
+		
+		};
+		while (count<0)
+		{
+			++added;
+			game_object::game_object gameobject{ game_object::create_game_object(gameobject_info) };
+			assert(gameobject.is_valid());
+			gameobjects.push_back(gameobject);
+			--count;
+		}
+	}
+	void remove_ramdom()
+	{
+
+	}
+
+
+	utl::vector<game_object::game_object> gameobjects;
+
+	flu32 added{ 0 };
+	flu32 removed{ 0 };
+	flu32 num_game_objects{ 0 };
+
 };
--- FluxionTestFramework/main.cpp
+++ FluxionTestFramework/main.cpp
@@ -1,5 +1,16 @@
 #define TEST_ECS 1
 
+#if _DEBUG
+// Must be defined before including stdlib.h / crtdbg.h so the CRT
+// records __FILE__ / __LINE__ for every allocation in the leak dump.
+#define _CRTDBG_MAP_ALLOC
+#include <stdlib.h>
+#include <crtdbg.h>
+#endif
+
+#pragma comment(lib,"engine.lib")
+
+
 #if TEST_ECS
 #include "Tests/ECS/ECSTest.h"
 #else
@@ -7,6 +18,11 @@
 #endif // TEST_ECS
 
 int main() {
+#if _DEBUG
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+#endif
+
+	
 	engine_test test{};
 
 	if (test.init()) {
