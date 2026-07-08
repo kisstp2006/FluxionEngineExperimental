55107270be6f7a9bf207c1298a3d2b9862aa0a16
TIGames <kisstp2006@gmail.com>
2026-07-08 15:52:59 +0200

    remove impl

--- FluxionTestFramework/Tests/ECS/ECSTest.h
+++ FluxionTestFramework/Tests/ECS/ECSTest.h
@@ -10,6 +10,10 @@
 using namespace fluxion;
 using namespace fluxion::ecs;
 
+// Shorter aliases for the nested types
+namespace game_object_ns = fluxion::ecs::game_object;
+namespace transform_ns   = fluxion::ecs::transform;
+
 // Every test header defines a class named engine_test so main.cpp
 // can instantiate the one selected by the TEST_* define.
 class engine_test : public test
@@ -48,25 +52,41 @@ private:
 			count = 1000;
 		}
 
-		transform::init_info transform_info{};
-		game_object::game_object_info info{ &transform_info };
+		transform_ns::init_info transform_info{};
+		game_object_ns::game_object_info info{ &transform_info };
 
 		while (count > 0)
 		{
 			++added;
-			game_object::game_object obj{ game_object::create_game_object(info) };
+			game_object_ns::game_object obj{ game_object_ns::create_game_object(info) };
 			assert(obj.is_valid());
 			game_objects.push_back(obj);
+			assert(game_object_ns::is_alive(obj));
 			--count;
 		}
 	}
 
 	void remove_random()
 	{
-		// TODO: remove a random batch of game objects
+		flu32 count = rand() % 30;
+		if (game_objects.size() < 1000) return;
+
+		while (count > 0)
+		{
+			const flu32 index{ (flu32)rand() % (flu32)game_objects.size() };
+			const game_object_ns::game_object gameobject{ game_objects[index] };
+			assert(gameobject.is_valid());
+			if (gameobject.is_valid()) {
+				game_object_ns::remove_game_object(gameobject);
+				game_objects.erase(game_objects.begin() + index);
+				assert(!game_object_ns::is_alive(gameobject));
+			}
+			--count;
+		}
 	}
 
-	utl::vector<game_object::game_object> game_objects;
+
+	utl::vector<game_object_ns::game_object> game_objects;
 
 	flu32 added{ 0 };
 	flu32 removed{ 0 };
